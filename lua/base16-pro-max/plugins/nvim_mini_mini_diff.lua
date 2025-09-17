5472cbc029982536c962b501d0cbd923370e6542
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-09-17 18:46:47 +0800

    feat(plugins.mini-diff): add `Over` highlights for overlay (#59)

--- lua/base16-pro-max/plugins/nvim_mini_mini_diff.lua
+++ lua/base16-pro-max/plugins/nvim_mini_mini_diff.lua
@@ -5,12 +5,42 @@ M.url = "https://github.com/nvim-mini/mini.diff"
 ---@param highlights table<string, vim.api.keyset.highlight>
 ---@param function_refs Base16ProMax.Config.HighlightGroups.FunctionRefs
 function M.setup(highlights, function_refs)
+  local get_group_color = function_refs.get_group_color_fn
+  local config = function_refs.styles_config
+  local c = function_refs.colors
+
   highlights.MiniDiffAdd = { link = "DiffAdd" }
-  highlights.MiniDiffChange = { link = "DiffChange" }
   highlights.MiniDiffDelete = { link = "DiffDelete" }
   highlights.MiniDiffSignAdd = { link = "DiffAdd" }
   highlights.MiniDiffSignChange = { link = "DiffChange" }
   highlights.MiniDiffSignDelete = { link = "DiffDelete" }
+
+  highlights.MiniDiffOverAdd = {
+    fg = get_group_color("diff", "added", c),
+    bg = get_group_color("diff", "added", c),
+    blend = config.blends.medium,
+  }
+  highlights.MiniDiffOverChange = {
+    fg = get_group_color("diff", "changed", c),
+    bg = get_group_color("diff", "changed", c),
+    blend = config.blends.medium,
+  }
+  highlights.MiniDiffOverChangeBuf = {
+    fg = get_group_color("diff", "changed", c),
+  }
+  highlights.MiniDiffOverContext = {
+    fg = get_group_color("foregrounds", "dark", c),
+    bg = get_group_color("diff", "changed", c),
+    blend = config.blends.medium,
+  }
+  highlights.MiniDiffOverContextBuf = {
+    fg = get_group_color("foregrounds", "dark", c),
+  }
+  highlights.MiniDiffOverDelete = {
+    fg = get_group_color("diff", "removed", c),
+    bg = get_group_color("diff", "removed", c),
+    blend = config.blends.medium,
+  }
 end
 
 return M
